10a0b46a876132b4a0fd0c26ca75adada485d3dd
Gabriela Barraza <gabu_uni@hotmail.com>
2017-06-19 10:25:20 -0300

    Datos de prueba con Factories y Faker

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,12 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Estudiante;
+use App\Profesor;
+use App\Curso;
+
+
+
 class DatabaseSeeder extends Seeder {
 
 	/**
@@ -12,8 +18,28 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		Model::unguard();
+		//sentencia que desabilita la verificacion de claves foraneas
+		//para poder truncar los modelos, esto es, antes de hacer la insercion
+		//borramos todo lo que tiene el modelo (datos) y luego insertamos
+		DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+		Estudiante::truncate();
+		Profesor::truncate();
+		Curso::truncate();
+		DB::table('curso_estudiante')->truncate();
 
+		//insercion de los elementos
+		factory(Profesor::class,50)->create();
+
+		factory(Estudiante::class,500)->create();
+
+		factory(Curso::class,40)->create(['profesor_id' => mt_rand(1,50)])
+		->each(function($curso)
+		{
+			//por cada instancia de un curso se asignara un conjunto aleatorio de estudiantes
+			$curso->estudiantes()->attach(array_rand(range(1,500),40));
+		});
+
+		
 		// $this->call('UserTableSeeder');
 	}
 
